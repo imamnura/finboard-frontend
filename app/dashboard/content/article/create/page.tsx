d54b7ff011a7e8acdea3c2dac5aa94a13f75fb17
imamnura <imam.12ra.kkpi@gmail.com>
2025-12-10 17:12:16 +0700

    fixing vulnerabilities

---
+++ app/dashboard/content/article/create/page.tsx
@@ -0,0 +1,8 @@
+export default function CreateArticlePage() {
+    return (
+        <div>
+            <h1>Create Article</h1>
+            {/* TODO: Implement create article form */}
+        </div>
+    );
+}
